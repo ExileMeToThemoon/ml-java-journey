fd76b64454f5bd8b331e708c4a9215c5dcff783f
wbking66 <wbking66@163.com>
2026-05-12 14:57:13 +0800

    feat: 类型提示还可以用在列表元组字典等中，可以用联合类型(|)声明一个变量是若干种类型的任意一种，也可以声明类型是none或者把类声明为变量的类型

--- newstudy1.py
+++ newstudy1.py
@@ -17,7 +17,43 @@ def get_name_with_age(name: str, age: int):
     return name_with_age
 print(get_name_with_age("John","17"))       #此处不会报错，是因为python强制类型转换了
 
-@app.get("/name/{name}")
-def get_name_with_age(name: str, age: int):
-    return {"name": name, "age": age}       #这里会报错了，因为在FastAPI里类型提示是强制执行的，会自动的转成声明的类型
-print(get_name_with_age("John","17"))
+# @app.get("/name/{name}")
+# def get_name_with_age(name: str, age: int):
+#     return {"name": name, "age": age}       #这里会报错了，因为在FastAPI里类型提示是强制执行的，会自动的转成声明的类型
+# print(get_name_with_age("John","17"))
+
+
+# 可以使用类星提示在列表元组字典等，以列表为例，冒号后先是列表，列表后的方括号写数据的类型str
+def process_items(items: list[str]):        #列表
+    for item in items:
+        print(item)
+
+def process_items(items_t: tuple[int, int, str], items_s: set[bytes]):  #元组
+    return items_t, items_s
+
+def process_items(prices: dict[str, float]):        #字典
+    for item_name, item_price in prices.items():
+        print(item_name)
+        print(item_price)
+# 联合类型：可以声明一个变量是若干种类型的任意一种，比如既可以是int也可以是str。定义时使用竖线(|)隔开
+def process_item(item: int | str):
+    print(item)
+# 可以声明一个值的类型是none;若传入名字打招呼，若为空输出hello world
+def say_hi(name: str | None = None):
+    if name is not None:
+        print(f"Hey {name}!")
+    else:
+        print("Hello World")
+
+say_hi()
+say_hi("John")
+
+
+# 类作为类型：
+# 你也可以把类声明为变量的类型。
+# 假设你有一个名为 Person 的类，带有 name：
+class Person:
+    def __init__(self, name: str):
+        self.name = name
+def get_person_name(one_person: Person):
+    return one_person.name
